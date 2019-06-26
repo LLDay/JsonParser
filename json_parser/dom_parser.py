a761964387a931cea59234924d35dc090f80fa25
LLDay <ssdenis99@gmail.com>
2019-06-26 23:17:41 +0300

    init commit

---
+++ json_parser/dom_parser.py
@@ -0,0 +1,5 @@
+import re
+
+def get_json_tree(filename):
+    file = open(filename, 'r')
+    
---
+++ json_parser/json_parser.py
@@ -0,0 +1,14 @@
+
+
+class JsonParser:
+    def __init__(self, filename):
+        pass
+
+    def __getitem__(self, key):
+        pass
+    
+    def __setitem__(self, key, value):
+        pass
+
+    def close(self):
+        pass
