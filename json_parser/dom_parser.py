f05a41d1b610bdb74835d9ef5d8d99928705b4ca
LLDay <ssdenis99@gmail.com>
2019-06-27 16:10:34 +0300

    Add LexTree builder and Tokens

--- json_parser/dom_parser.py
+++ json_parser/dom_parser.py
@@ -1,5 +1,57 @@
 import re
 
+class LexTree:
+    def __init__(self):
+        self.root = {}
+        self.current = root
+        self.trace = []
+
+    def add_object(self, key_name):
+        self.current[key_name] = {}
+        self.trace.append(self.current)
+        self.current = self.current[key_name]
+
+    def add_value(self, key, value):
+        self.current[key] = value
+
+    def back(self):
+        self.current = trace.pop()
+
+    def start(self):
+        self.current = self.root
+        self.trace = []
+
+    def to(self, key):
+        start()
+
+        for k in key.split('.'):
+            self.trace.append(self.current)
+            self.current = self.current[k]
+
+
+class Token:
+    def __init__(self, name, regex, content_group = 0):
+        self.name = name
+        self.regex = r'[ \n\t]*' + regex
+        self.last_size = 0
+        self.content_group = content_group
+
+    def __contains__(self, line):
+        result = re.match(self.regex)
+        if result:
+            self.last_size = len(result.group(0))
+            return True
+        else:
+            return False
+
+    def get_content(self, line):
+        return re.match(self.regex).group(self.content_group)
+
 def get_json_tree(filename):
-    file = open(filename, 'r')
-    
+    key = Token('key', r'\"(.*)\"[\n\t ]*:', 1)
+    opo = Token('opo', r'\{')
+    clo = Token('clo', r'\}')
+    opl = Token('opl', r'\[')
+    cll = Token('cll', r'\]')
+    sep = Token('sep', r',') 
+
--- json_parser/json_parser.py
+++ json_parser/json_parser.py
@@ -5,10 +5,24 @@ class JsonParser:
         pass
 
     def __getitem__(self, key):
+        '''
+        Get a value by `key`
+        '''
         pass
     
     def __setitem__(self, key, value):
+        '''
+        Set or create new record
+        '''
         pass
+
+    
+    def __contains__(self, key):
+        '''
+        Check if the json-file contains a `key`
+        '''
+        pass
+
 
     def close(self):
         pass
