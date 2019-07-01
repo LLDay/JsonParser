18cbfc9e75f0d5c98496fe0fb7d13365202e09ed
LLDay <ssdenis99@gmail.com>
2019-07-01 14:32:38 +0300

    Add json builder class

--- json_parser/json_tree.py
+++ json_parser/json_tree.py
@@ -1,28 +1,47 @@
-    
+
 
 class JsonTreeBuilder:
     def __init__(self):
         self._root = {}
         self._current = self._root
         self._trace = []
+        self._pointer_key = 'root'
 
 
     def add_key(self, key_name):
-        pass
+        self._pointer_key = key_name
 
     def add_object(self):
-        pass
+        self._trace.append(self._current)
+        if isinstance(self._current, list):
+            self._current.append({})
+            self._current = self._current[-1]
+        else:
+            self._current[self._pointer_key] = {}
+            self._current = self._current[self._pointer_key]
 
     def add_list(self):
-        pass
+        self._trace.append(self._current)
+        if isinstance(self._current, list):
+            self._current.append([])
+            self._current = self._current[-1]
+        else:
+            self._current[self._pointer_key] = []
+            self._current = self._current[self._pointer_key]
 
     def add_value(self, value):
-        pass
+        if isinstance(self._current, list):
+            self._current.append(value)
+        else:
+            self._current[self._pointer_key] = value
 
+    def get_tree(self):
+        if self._current != self._root:
+            raise RuntimeError('Json has errors')
+        return self._root
 
     def back(self):
-        self._current = _trace.pop()
-
+        self._current = self._trace.pop()
 
     def start(self):
         self._current = self._root
