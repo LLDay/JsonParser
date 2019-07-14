499271d0963a856a2adbf05aef180f4446ad4ffc
LLDay <ssdenis99@gmail.com>
2019-07-14 19:37:50 +0300

    Add writing tests

--- test/json_parser_test.py
+++ test/json_parser_test.py
@@ -1,35 +1,55 @@
 import unittest
-from json_parser.json_file import JsonFile
+from shutil import copy2
+from json_parser import *
 
 class JsonTester(unittest.TestCase):
     
     def setUp(self):
-        self.t1 = JsonFile('test/jsons/t1.json')
-        self.t2 = JsonFile('test/jsons/t2.json')
+        pass
 
     def tearDown(self):
-        self.t1.close()
-        self.t2.close()
+        pass
 
     def test_reading(self):
-        sr = str(self.t1['configurations'])
-        self.assertEqual(self.t1.__len__(), 2)
-        self.assertEqual(len(self.t1), 2)
-        self.assertEqual(len(self.t2), 5)
+        t1 = JsonFile('test/jsons/t1.json')
+        t2 = JsonFile('test/jsons/t2.json')
+        self.assertEqual(len(t1), 2)
+        self.assertEqual(len(t2), 5)
 
-        self.assertEqual(self.t1['version'], '0.2.0')
-        self.assertEqual(self.t1['configurations']['name'], 'Json_Parser')
-        self.assertEqual(self.t1.d['configurations.name'], 'Json_Parser')
-        self.assertIsNone(self.t1.d['configurations.subauthors'])
+        self.assertEqual(t1['version'], '0.2.0')
+        self.assertEqual(t1['configurations']['name'], 'Json_Parser')
+        self.assertEqual(t1.d['configurations.name'], 'Json_Parser')
+        self.assertIsNone(t1.d['configurations.subauthors'])
 
-        self.assertEqual(self.t2._0["index"], 0)
-        self.assertEqual(self.t2._1["index"], 1)
+        self.assertEqual(t2._0["index"], 0)
+        self.assertEqual(t2._1["index"], 1)
 
-        self.assertEqual(self.t2._0['friends']._1['name'], 'Burnett Salinas')
-        self.assertTrue(self.t2._3['latitude'] < -40)
-        self.assertTrue(self.t2[3]['isActive'])
+        self.assertEqual(t2._0['friends']._1['name'], 'Burnett Salinas')
+        self.assertTrue(t2._3['latitude'] < -40)
+        self.assertTrue(t2[3]['isActive'])
 
-        self.assertTrue(self.t2.d['3.isActive'])
+        self.assertTrue(t2.d['3.isActive'])
+        
+        t1.close()
+        t2.close()
+
+    def test_writing(self):
+        copy2('test/jsons/t1.json', 'test/jsons/t3.json')
+        t3 = JsonFile('test/jsons/t3.json')
+        t3['version'] = '0.3.0'
+        t3['new line'] = "I'm new line"
+        t3['new object'] = JsonObject({'Hello': 'world'}, length=12.5)
+        t3['new object']['temperature'] = 120.8
+        t3['new list'] = JsonList([True, False, None])
+        t3['new list'].extend([True, "Hello", 12, 0.3])
+        t3['empty object'] = JsonObject()
+        t3['empty list'] = JsonList() 
+        t3.close()
+
+        # t1_file = open('test/jsons/t3.json')
+        # all_text = t1_file.read()
+        # self.assertRegexpMatches(all_text, r'.*"version": "0.3.0".*')
+        # self.assertRegexpMatches(all_text, r'.*"new line": "I\'m new line"')
         
 if __name__ == '__main__':
     unittest.main()
